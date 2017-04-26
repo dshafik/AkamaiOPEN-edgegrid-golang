ca6e3741d3fba58a93489aa67adb45d6cd96bdb5
Davey Shafik <me@daveyshafik.com>
2017-04-26 08:40:21 -0700

    Complete PAPI rule format support
    - Add godoc
    - Add `PapiRuleFormats.GetRuleFormats()`

--- edgegrid/papi_v0_ruleformats.go
+++ edgegrid/papi_v0_ruleformats.go
@@ -1,15 +1,43 @@
 package edgegrid
 
+// PapiRuleFormats is a collection of available rule formats
 type PapiRuleFormats struct {
 	resource
+	service     *PapiV0Service
 	RuleFormats struct {
 		Items []string `json:"items"`
 	} `json:"ruleFormats"`
 }
 
-func NewPapiRuleFormats() *PapiRuleFormats {
-	ruleFormats := &PapiRuleFormats{}
+// NewPapiRuleFormats creates a new PapiRuleFormats
+func NewPapiRuleFormats(service *PapiV0Service) *PapiRuleFormats {
+	ruleFormats := &PapiRuleFormats{service: service}
 	ruleFormats.Init()
 
 	return ruleFormats
+}
+
+// GetRuleFormats populates PapiRuleFormats
+//
+// API Docs: https://developer.akamai.com/api/luna/papi/resources.html#listruleformats
+// Endpoint: GET /papi/v0/rule-formats
+func (ruleFormats *PapiRuleFormats) GetRuleFormats() error {
+	res, err := ruleFormats.service.client.Get("/papi/v0/rule-formats")
+
+	if err != nil {
+		return err
+	}
+
+	if res.IsError() {
+		return NewAPIError(res)
+	}
+
+	newRuleFormats := NewPapiRuleFormats(ruleFormats.service)
+	if err := res.BodyJSON(newRuleFormats); err != nil {
+		return err
+	}
+
+	*ruleFormats = *newRuleFormats
+
+	return nil
 }
